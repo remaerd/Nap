174a0f77996eabc4d6fc061a9f3e21c52ba2be9a
Sean Cheng <sean@extremelylimited.com>
2015-03-28 14:04:42 +0800

    Initial Commit

---
+++ Nap/Nap.h
@@ -0,0 +1,19 @@
+//
+//  Nap.h
+//  Nap
+//
+//  Created by 郑行之 on 3/28/15.
+//  Copyright (c) 2015 Extremely Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nap.
+FOUNDATION_EXPORT double NapVersionNumber;
+
+//! Project version string for Nap.
+FOUNDATION_EXPORT const unsigned char NapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nap/PublicHeader.h>
+
+
